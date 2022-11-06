eb8dd689d8c6f5340917d2300cddcc969b38f944
Veredux <valentin@hesse-online.info>
2022-11-06 17:14:23 +0100

    061122

--- like_button.js
+++ like_button.js
@@ -4,24 +4,15 @@ const e = React.createElement;
 // Teilnehmer Button
 
 let jsonData = {
-    "users": [
-        {
-            "name": "alan", 
-            "age": 23,
-            "username": "aturing"
-        },
-        {
-            "name": "john", 
-            "age": 29,
-            "username": "__john__"
-        }
+    "metadata": [
+        Date.now()
     ]
 }
 
 class Button extends React.Component {
 
      cock() {
-        fetch('https://ptsv2.com/t/alelelelale/post', {
+        fetch('https://ptsv2.com/t/r4qqa-1667579801/post', {
             method: 'POST',
             mode: 'cors',
             body: JSON.stringify(jsonData)
@@ -31,11 +22,11 @@ class Button extends React.Component {
     render() {
       return (
         <button className="bbutton" onClick={this.cock}>
-            CoCK
         </button>
       );
     }
 }
+
 
 const domContainer = document.querySelector('#Teilnehmer-Button');
 const root = ReactDOM.createRoot(domContainer);
